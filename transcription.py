38669c4aafc99ed5c3418f2cb1be99c7b8c52b2d
LKnopf <lars.knopf@gmx.de>
2017-05-30 12:22:28 +0200

    added play binding on DNA

--- transcription.py
+++ transcription.py
@@ -7,6 +7,7 @@ from molecules import Ribo, Protein, MRNA, PopulationCollection, ParticleCollect
 class Transcription(processes.Process):
     #The Transcription process
     def __init__(self, name, model):
+        self.test = (DNA("DNA", "ATGCTGATGAC"))
         # call the constructor of the base class (processes.Process in this case)
         super().__init__(name, model)
 
@@ -14,6 +15,19 @@ class Transcription(processes.Process):
         """
         check for free polymerase
         """
-        print(self.model.states[Polymerase].molecules)
+        #self.model.states[Polymerase].molecules
+
+        s = self.model.states[DNA].get_molecules("DNA")
+        s = self.model.states[DNA].molecules["DNA"]
+        #print(s)
+        self.test.bind_polymerase(len(self.model.states[Polymerase].molecules))
+        print(self.test.poly_pos)
+
+
+
+
+
+
+
 
     
