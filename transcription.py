444ccf9b73e1e3f3173975117dbaa061f694681e
Bernhard <liepeber@student.hu-berlin.de>
2017-05-31 13:50:44 +0200

    modified for cheching if collission check works

--- transcription.py
+++ transcription.py
@@ -22,13 +22,22 @@ class Transcription(processes.Process):
 
 
 
-        DNA_obj.bind_polymerase(self.model.states[Polymerase].molecules["free Polymerase"])
+        #DNA_obj.bind_polymerase(self.model.states[Polymerase].molecules["free Polymerase"])
 
-        for i in range(500):
+        for i in range(1):   #500
+            DNA_obj.bind_polymerase(self.model.states[Polymerase].molecules["free Polymerase"])
+            pass
+
+
+
+        for i in range(1):   #500
             DNA_obj.move_polymerase()
+            
+
+
 
         #print(self.test.poly_status)
-        print(DNA_obj.poly_pos)
+        #print(DNA_obj.poly_pos)
         
 
 
